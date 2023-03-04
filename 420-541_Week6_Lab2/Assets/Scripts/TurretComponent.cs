195998540111f0da347303dc91965e7ae8212c46
Jeffrey <jeffreygrospe@yahoo.com>
2023-03-03 22:14:03 -0500

    add turret rotation
    buggy but it rotates and follows the player

--- 420-541_Week6_Lab2/Assets/Scripts/TurretComponent.cs
+++ 420-541_Week6_Lab2/Assets/Scripts/TurretComponent.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
+using UnityEngine.SocialPlatforms;
 using UnityEngine.UI;
 
 public class TurretComponent : MonoBehaviour
 {
-    public float maxAngle = 45;
+    public float maxAngle = 45f;
     private float damage = 0.1f;
     public Slider hpBar;
 
@@ -12,16 +13,34 @@ public class TurretComponent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 direction = player.position- transform.position;
-        Debug.DrawRay(transform.position, direction, Color.green);
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 90;
-        Debug.Log("angle " + angle);
+        // direction to face
+        Vector3 direction = player.position - transform.position;
 
-        // rotation
-        //Quaternion angleAxis = Quaternion.AngleAxis(angle, Vector3.up);
-        //transform.rotation = Quaternion.Slerp(transform.rotation, angleAxis, Time.deltaTime * 50);
-        // using Euler
-        transform.eulerAngles = Vector3.up * angle;
+        Debug.DrawRay(transform.position, direction, Color.green);
+
+ 
+        float angle = Mathf.Acos(Vector3.Dot(direction.normalized, transform.forward.normalized)) * Mathf.Rad2Deg;
+
+        Debug.Log("angle: " + angle);
+
+        if (angle <= maxAngle)
+        {
+            Debug.Log("kill");
+            Quaternion targetRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 5f * Time.deltaTime);
+        }
+
+        //// rotation
+        ////Quaternion angleAxis = Quaternion.AngleAxis(angle, Vector3.up);
+        ////transform.rotation = Quaternion.Slerp(transform.rotation, angleAxis, Time.deltaTime * 50);
+        //// using Euler
+      //transform.eulerAngles = Vector3.up * angle;
+        //transform.rotation = Quaternion.LookRotation(Vector3.forward * angle);
+
+        //float d = Vector3.Dot(transform.forward, player.forward);
+        //float a = Mathf.Acos(d) * Mathf.Rad2Deg;
+      
+        
     }
 
     // todo make a method to rotate the turrent 45 deg towards the player
